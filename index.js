9dbbb5050235ff9f5391f18e244f072e8bdb8d9d
ValaA88t <vala.arefi@gmail.com>
2023-03-13 16:49:40 +0100

    Add first version

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-const a = 1;
+const emoji = require('node-emoji');
 
-console.log('hello');
+// console.log(process.argv[2]);
+
+if (process.argv[2] === undefined) {
+  console.log(emoji.random().emoji);
+} else if (emoji.hasEmoji(process.argv[2])) {
+  console.log(emoji.get(process.argv[2]));
+} else {
+  console.log('emoji not found');
+}
